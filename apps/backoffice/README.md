f840dfff8ecc38ae06d3f7567c9a072d72943a1a
Tristan Debroise <tristandebroise@gmail.com>
2025-11-13 18:42:38 +0100

    Fixes

--- apps/backoffice/README.md
+++ apps/backoffice/README.md
@@ -188,3 +188,4 @@ The project uses Tailwind CSS with shadcn/ui components. All components follow t
 Private - EloHero Platform
 
 
+
--- apps/backoffice/src/components/ui/table.tsx
+++ apps/backoffice/src/components/ui/table.tsx
@@ -89,3 +89,4 @@ TableCaption.displayName = 'TableCaption';
 
 export { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption };
 
+
