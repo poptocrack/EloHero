256ce643d29edd303f912aef68e1a9d80e7234c6
Tristan Debroise <tristandebroise@gmail.com>
2025-11-14 23:24:50 +0100

    ?

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,8 @@
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
+// getSentryExpoConfig already extends Expo's default config
+// It should properly handle the entry point from package.json
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
+
